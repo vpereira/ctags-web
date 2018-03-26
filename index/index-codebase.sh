b2cba6dbb64719ea562774c33a36b1af6dfe19c3
Victor Pereira <vpereira@suse.de>
2018-03-26 13:41:31 +0200

    index-codebase ready

--- index/index-codebase.sh
+++ index/index-codebase.sh
@@ -5,8 +5,13 @@ if [ "$1" == "" ]; then
   exit -1
 fi
 
-# one container per codestream
-for d in `find $1 -maxdepth 1 -type d`; do
-  docker run -ti --rm --network="ctagsweb_default" -v "$PWD:/ctags-web" -v "$1:$1" ctags-web/web bash -c "ctags --recurse=yes --fields=* --output-format=json -f $d.json $d && \
-                                /ctags-web/index/index db $d.json  && echo $?"
+# one container per package
+for d in `find $1 -mindepth 2 -maxdepth 2 -type d`; do
+  echo $d
+  # $d is full path
+  LNAME=`echo $d | sed -e 's/^\///' | sed -e 's/:\|\//_/g'`
+  JSON_PATH=$1/$LNAME.json
+  echo $JSON_PATH
+  docker run --rm --name $LNAME --network="ctagsweb_default" -v "$PWD:/ctags-web" -v "$1:$1" -d ctags-web/web bash -c "ctags --recurse=yes --fields=* --output-format=json -f $JSON_PATH $d &&  /ctags-web/index/index db $JSON_PATH && rm -f $JSON_PATH"
+  sleep $[ ( $RANDOM % 2 )  + 1 ]s
 done
